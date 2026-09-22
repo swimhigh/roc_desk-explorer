a90ea27709d05a769b00f0ad299e26454781b87e
lipeng <lipeng@szkingdom.com>
2026-09-22 19:12:52 +0800

    fix: embed standalone frontend in release build

--- build-standalone.bat
+++ build-standalone.bat
@@ -2,7 +2,7 @@
 setlocal
 cd /d "%~dp0"
 echo Building roc_desk-explorer (Release)...
-cargo build --release -p roc_desk_explorer_standalone
+cargo build --release --features custom-protocol -p roc_desk_explorer_standalone
 if errorlevel 1 (
   echo BUILD FAILED: roc_desk-explorer
   exit /b 1
