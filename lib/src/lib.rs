f6cedc7e956a86472de29852f131dbf192f9a3fb
lipeng <lipeng@szkingdom.com>
2026-09-22 13:05:49 +0800

    Add independent library and standalone shell

---
+++ lib/src/lib.rs
@@ -0,0 +1,4 @@
+﻿//! Resource explorer library boundary.
+//! Business source is migrated under src/ and is wired into this crate as
+//! host adapters are replaced with roc_desk-common interfaces.
+pub const TOOL_NAME: &str = "roc_desk-explorer";
---
+++ standalone/src/main.rs
@@ -0,0 +1,3 @@
+﻿fn main() {
+    println!("roc_desk-explorer standalone shell");
+}
